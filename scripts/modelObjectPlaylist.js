3d95621f46ab701dce9cdd3e8316be0efa06b13c
Tancredi <tancredi.dangelo.22@gmail.com>
2026-04-21 01:53:13 +0200

    Update modelObjectPlaylist.js

---
+++ scripts/modelObjectPlaylist.js
@@ -0,0 +1,24 @@
+
+myPlaylist = {
+  id: "",
+  name: "",
+  creator: "",
+  description: "",
+  isPublic: true,
+  lastUpdated: "",
+  stats: {
+    followerCount: 0,
+    totalDurationSeconds: 0
+  },
+  // The 'tracks' property is an array of song objects
+  tracks: [
+    {
+      trackId: "",
+      title: "",
+      artist: "",
+      album: "",
+      duration: "",
+      addedAt: ""
+    }
+  ]
+};
