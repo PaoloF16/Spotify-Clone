37cc681c094ea707943e5084e8c0b194c556d53e
Tancredi <tancredi.dangelo.22@gmail.com>
2026-04-20 16:13:28 +0200

    basic fetch

--- scripts/script.js
+++ scripts/script.js
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 const getArtist = function() {
     fetch("https://striveschool-api.herokuapp.com/api/deezer/artist/412")
         .then((response) => {
@@ -30,3 +31,55 @@ const getAlbum = function() {
 
 getAlbum()
 getArtist()
+=======
+
+class Artist {
+    
+}
+
+
+const getArtist = function() {
+
+    fetch("https://striveschool-api.herokuapp.com/api/deezer/artist/412")
+
+    .then((response) => { 
+        if (response.ok) {
+            console.log(response)
+            return response.json()
+            } else {
+                throw new Error(`Request failed: ${response.status}`)   
+            }
+        })
+
+    .then((data) => {
+        console.log(data)
+    })
+
+    .catch((error) => console.log(error))
+
+    }
+
+const getAlbum = function() {
+
+    fetch("https://striveschool-api.herokuapp.com/api/deezer/album/75621062")
+
+    .then((response) => { 
+        if (response.ok) {
+            console.log(response)
+            return response.json()
+            } else {
+                throw new Error(`Request failed: ${response.status}`)   
+            }
+        })
+
+    .then((data) => {
+        console.log(data)
+    })
+
+    .catch((error) => console.log(error))
+
+    }
+
+
+
+>>>>>>> Stashed changes
